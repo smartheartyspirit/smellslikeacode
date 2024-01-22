babfb437e1f923b62a67b11af7dbb93da8099eeb
smartheartyspirit <gigagagua923@gmail.com>
2024-01-22 18:15:02 +0000

    Creating middlesex

---
+++ 22012024_181502_middlesex_sn2em.sh
@@ -0,0 +1,1 @@
+Bill Bowerman, the co-founder of the shoe company Nike, got his first shoe idea after staring at a waffle iron. This gave him the idea of using squared spikes to make the shoes lighter
