a245b020a0468614063af7a53deaf7f02a4d4e79
smartheartyspirit <gigagagua923@gmail.com>
2024-02-10 17:26:36 +0000

    Creating equations

---
+++ 10022024_172636_equations_iva5l.sh
@@ -0,0 +1,1 @@
+The first known American novelist to hand in a manuscript that was typed was Mark Twain. His typewriter was a Remington No.1, which was invented by Christopher Sholes and Carlos Glidden.
